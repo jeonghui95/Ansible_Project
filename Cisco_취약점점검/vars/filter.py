67b1b6d66360b6019f681e6cd886101f1fc167b1
LAPTOP-CD61PRT2\rhkwk <rhkwk3333@gmail.com>
2025-03-26 17:31:04 +0900

    [#1] refactor(role): 전체적인 코드 refactoring
    1. filter.py 수정
    2. task  debug module 추가

--- Cisco_취약점점검/vars/filter.py
+++ Cisco_취약점점검/vars/filter.py
@@ -7,10 +7,29 @@ import re
 
 hostname = os.environ.get('hostname',' ')
 host = os.environ.get('host',' ')
-content = os.environ.get('content',' ')
+total_item = os.environ.get('total_item',' ')
 message = os.environ.get('message',' ')
 iid = os.environ.get('id',' ')
 value = os.environ.get('value',' ')
+name = os.environ.get('name',' ')
+command = os.environ.get('command',' ')
+system = os.environ.get('system',' ')
+report_type = os.environ.get('report_type',' ')
+
+
+# merged_dict --------------------------------------------------------
+
+result_dict = {
+
+    'hostname': hostname,
+    'host': host,
+    'command': command,
+    'name': name,
+    'report_type': report_type,
+    'system': system,
+    'id': iid,
+    'message': message
+}
 
 
 
@@ -18,19 +37,21 @@ value = os.environ.get('value',' ')
 #필터 함수 -----------------------------------------------------------
 
 
-def filter(content,value):
+def filter(total_item,value):
 
-    if content:
-        is_successsed = '0'
+    if total_item:
+        total_item = f'{total_item}'
+    
     else:
         return {
-            "is_passed" : None,
-            "is_successed" : None,
-            "fail_message" : None,
+
+            is_passed: "None",
+            is_successed: 0, 
+            message: '명령에 실패했습니다'
 
         }
-
     
+    fail_items = []
     match value:
 
         case 'int':
@@ -45,11 +66,19 @@ def filter(content,value):
             for chunk in chunk_list:
                 number = chunk[3]
                 is_passed = '0' if float(number) < 7 else '1'
+                fail_item = 'fail' if is_passed == '1' else ' '
             
+            is_successed = '0' if is_passed == '0' else '1'
     
+    message = f'  결과| name : {name}  \n total_item: {total_item}'
+
+    if fail_item:
+        fail_items.append(fail_item)
+
     return  {
             "is_passed" : is_passed,
-            "is_successed" : is_successsed,
+            "is_successed" : is_successed,
+            "message": message
         }
 
        
@@ -61,17 +90,9 @@ def filter(content,value):
 
 #---------------------------------------------------------------------
 
-result_dict = {
-
-"hostname" :hostname,
-"host": host,
-"message": message,
-"id" : iid,
-
-}
+         
+result_dict = result_dict.update(filter(total_item,value))
             
-result_dict = result_dict.update(filter(content,value))
-            
-print(json.dumps(result_dict))
+print(json.dumps(result_dict , ensure_ascii=False))
             
             
